1659c749937617683089dde261eb4f8e13523870
OmarReyesC <omareyescar@gmail.com>
2024-08-23 17:32:51 -0600

    Class 6 completed

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,13 @@ import './TodoSearch.css';
 
 function TodoSearch() {
     return (
-        <input placeholder="Pasear a Oreo" className='TodoSearch'/>
+        <input placeholder="Pasear a Oreo" className='TodoSearch' onChange={(event) => {
+            console.log('Escribiste en el TodoSearch');
+            console.log(event);
+            console.log(event.target);
+            console.log(event.target.value);
+            }}
+        />
     );
 }
 
