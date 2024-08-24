654088eb49c6639a22bb1a3d4397fe042ab67269
OmarReyesC <omareyescar@gmail.com>
2024-08-23 18:49:47 -0600

    Class 7 completed

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
+    console.log('Los usuarios buscan ' + searchValue);
+
     return (
-        <input placeholder="Pasear a Oreo" className='TodoSearch' onChange={(event) => {
-            console.log('Escribiste en el TodoSearch');
-            console.log(event);
-            console.log(event.target);
-            console.log(event.target.value);
+        <input placeholder="Pasear a Oreo" className='TodoSearch' 
+        value={searchValue}
+        onChange={(event) => {
+            setSearchValue(event.target.value);
             }}
         />
     );
