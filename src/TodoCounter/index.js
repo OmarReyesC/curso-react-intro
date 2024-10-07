f7b7b3f9a9671be72a81c279d4180b8b9ff70802
OmarReyesC <omareyescar@gmail.com>
2024-10-06 19:56:09 -0600

    Class 23 completed

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
 
-function TodoCounter({completed, total}) {
+function TodoCounter() {
+    const {
+        completedTodos,
+        totalTodos,
+    } = React.useContext(TodoContext);
+
     return (
         <h1 className='TodoCounter'>
-            Has completado <span>{completed}</span> de <span>{total}</span> ToDos
+            Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span> ToDos
         </h1>
     );
 };
